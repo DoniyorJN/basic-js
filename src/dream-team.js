db2eec21f2a7701dfeccdee91ba729d18dcb5df7
DoniyorJN <umidshoot@gmail.com>
2020-03-17 17:36:41 +0500

    feat: add solution dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,6 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  return (
+    members.map((elem) => {
+    if (typeof elem === 'string') return elem.trim()[0].toUpperCase();}).sort().join(''));
+  };
